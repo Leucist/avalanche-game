bcdd8832241799df4804d73fd740e3bd0e171b95
Nazar Hrinchenko <grinnazar@gmail.com>
2024-11-07 14:02:36 +0100

    Created an interface InputController, created InputController class, ActionType with enum

---
+++ Avalanche.Core/AcionType.cs
@@ -0,0 +1,17 @@
+﻿namespace Avalanche.Core
+{
+   public enum ActionType
+   {
+        Up,
+        Down,
+        Left,
+        Right,
+        StraightAttack,
+        SplashAttack,
+        Shoot,
+        Interact,
+        Enter,
+        NullAction
+
+   }
+}
