cb6c450cbfe7f33692dac8a7581a323eab61562e
Nazar Hrinchenko <grinnazar@gmail.com>
2024-11-11 00:52:36 +0100

    Important commit

---
+++ Avalanche.Console/ConsoleCutscene.cs
@@ -0,0 +1,31 @@
+﻿using Avalanche.Core;
+using Avalanche.Core.Interfaces;
+
+
+namespace Avalanche.Console
+{
+    public class ConsoleCutscene
+    {
+        public string localPath = "CutScenes\\";
+        public void Render(string artName)
+        {
+            string projectPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..");
+            string filePath = Path.Combine(projectPath,localPath, artName);
+
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+
+                foreach (string line in lines)
+                {
+                    System.Console.WriteLine(line);
+                }
+            }
+
+            catch (Exception ex)
+            {
+               System.Console.WriteLine("Error reading the file: " + ex.Message);  // I'm not chatGPT, trust me, bro
+            }
+        }
+    }
+}
