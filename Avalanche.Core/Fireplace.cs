22d3b7d391d287c93a21871a31243cd6eee96550
Nazar Hrinchenko <grinnazar@gmail.com>
2024-11-12 15:49:36 +0100

    Created campfire

---
+++ Avalanche.Core/Fireplace.cs
@@ -0,0 +1,24 @@
+﻿using static Avalanche.Core.AppConstants;
+
+namespace Avalanche.Core
+{
+    public class Fireplace : GameObject
+    {
+        Player player;
+        bool isUsed;
+        public Fireplace(int x, int y, Player p) : base(x, y)
+        {
+            player = p;
+            isUsed = true;
+        }
+
+        public void UseCampfire()
+        {
+            if (isUsed)
+            {
+                player._heat = DefaultPlayerHeat;
+            }
+            isUsed = false;
+        }
+    }
+}
