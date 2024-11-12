f30040afc33626185c3c9d7e9d09059f2102f62f
= <mr.multik.zzz@gmail.com>
2024-11-12 14:41:18 +0100

    hp

--- Avalanche.Console/ConsoleLevelView.cs
+++ Avalanche.Console/ConsoleLevelView.cs
@@ -1,4 +1,5 @@
-using Avalanche.Core;
+﻿using Avalanche.Core;
+using System.Runtime.CompilerServices;
 using static Avalanche.Core.AppConstants;
 
 namespace Avalanche.Console
@@ -7,16 +8,51 @@ namespace Avalanche.Console
     {
         private readonly LevelModel _model;
         private bool _wasNeverDrawn;
-        
-        public ConsoleLevelView(LevelModel model) {
+        private int _previousHealthPointsCount;
+
+        public ConsoleLevelView(LevelModel model)
+        {
             _model = model;
+            _previousHealthPointsCount = _model._player._health;
             _wasNeverDrawn = true;
         }
 
-        public void Render() {
-            if (_wasNeverDrawn) {
+        public void DrawHP()
+        {
+
+            if (_model._player._health != _previousHealthPointsCount)
+            {
+
+                _previousHealthPointsCount = _model._player._health;
+            }
+
+
+            System.Console.SetCursorPosition(6, 9);
+            System.Console.Write("Health Points: ");
+            System.Console.ForegroundColor = ConsoleColor.Red;
+            //System.Console.WriteLine(new string('♥️', _model._player._health));
+            if (_model._player._health > 10)
+            {
+                _model._player._health = 10;
+            }
+
+            for (int i = 0; i < _model._player._health; i++)
+            {
+                System.Console.Write("♥ ");
+            }
+
+
+            System.Console.ForegroundColor = ConsoleColor.White;
+
+        }
+
+        public void Render()
+        {
+            if (_wasNeverDrawn)
+            {
                 // Clear screen and draw room borders
                 ConsoleRenderer.ClearScreen();
+                DrawHP();
                 ConsoleRenderer.DrawBox(RoomCharWidth, RoomCharHeight);
                 _wasNeverDrawn = false;
                 RenderDoors();
@@ -25,13 +61,14 @@ namespace Avalanche.Console
             ConsoleRenderer.ClearDirtyPixels(_model._currentRoom!._dirtyPixels);
             _model._currentRoom!._dirtyPixels.Clear();
 
-            if (_model._currentRoom!._isDirty) {
+            if (_model._currentRoom!._isDirty)
+            {
                 RenderDoors();
                 // Render Player
                 ConsoleRenderer.DrawPlayer(
                     _model._player.GetX(),
                     _model._player.GetY());
-                
+
                 // Render each of the enemies
                 List<Entity> enemies = _model._currentRoom._enemies;
                 foreach (Entity enemy in enemies)
@@ -42,8 +79,10 @@ namespace Avalanche.Console
             }
         }
 
-        private void RenderDoors() {
-            foreach (var door in _model._currentRoom!._doors) {
+        private void RenderDoors()
+        {
+            foreach (var door in _model._currentRoom!._doors)
+            {
                 ConsoleRenderer.DrawDoor(door.Key.GetX(), door.Key.GetY(), door.Value._isLevelExit);
             }
         }
