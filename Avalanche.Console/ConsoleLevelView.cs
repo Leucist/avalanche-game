876b3347b4331e643a98fd31b16946f468ebf18f
leucistic <leucist@yandex.by>
2024-11-14 17:05:32 +0100

    HP loss marking

--- Avalanche.Console/ConsoleLevelView.cs
+++ Avalanche.Console/ConsoleLevelView.cs
@@ -63,7 +63,7 @@ namespace Avalanche.Console
                 }
                 System.Console.ForegroundColor = ConsoleColor.White;
 
-                _previousHealthPointsCount = _model._player._health;
+                // _previousHealthPointsCount = _model._player._health;
             }
         }
 
@@ -276,9 +276,15 @@ namespace Avalanche.Console
             {
                 RenderDoors();
                 // Render Player
+                if (_model._player._health != _previousHealthPointsCount) {
+                    System.Console.BackgroundColor = ConsoleColor.Red;
+                    _previousHealthPointsCount = _model._player._health;
+                }
                 ConsoleRenderer.DrawPlayer(
                     _model._player.GetX(),
                     _model._player.GetY());
+                System.Console.ResetColor();
+                System.Console.ForegroundColor = ConsoleColor.White;
 
                 // Render each of the enemies
                 List<Enemy> enemies = _model._currentRoom._enemies;
