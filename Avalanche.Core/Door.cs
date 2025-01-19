5296c284b7bfdd90522761824026d502409f852e
leucistic <leucist@yandex.by>
2025-01-19 11:05:13 +0100

    Updated Door constructor - closed by default now

--- Avalanche.Core/Door.cs
+++ Avalanche.Core/Door.cs
@@ -9,7 +9,7 @@ namespace Avalanche.Core
 
         public Door(int doorID, int id1, int id2, bool isLevelExit) {
             _ID = doorID;
-            _isClosed = false;
+            _isClosed = true;
             _isLevelExit = isLevelExit;
             _betweenRoomsOfID = [id1, id2];
         }
