d0f2fb1455ef47c9f0329664ef23b98edca8f386
leucistic <leucist@yandex.by>
2025-01-21 15:53:28 +0100

    Difficulty fix

--- Avalanche.Core/OptionsModel.cs
+++ Avalanche.Core/OptionsModel.cs
@@ -5,13 +5,14 @@
         private const string _menuTitle = "Options";
 
         private readonly int _amountOfDifficultyLevels = Enum.GetValues(typeof(DifficultyLevelType)).Length;
-        private int _difficulty = 0;
+        private int _difficulty = (int) GameState._difficulty;
         
         public int Difficulty => _difficulty;
 
         private void SwitchDifficulty(int direction) {
             // Rise or Lower the Difficulty and ensure it stays within bounds
-            _difficulty = (_difficulty + direction + _amountOfDifficultyLevels) % _amountOfDifficultyLevels;
+            _difficulty = (_difficulty + direction) % (_amountOfDifficultyLevels + 1);
+            if (_difficulty == 0) _difficulty = _amountOfDifficultyLevels;
         }
 
         public void RiseDifficulty() {
